559f91cf73594bf33e0b77ee7b63dbebe01e058e
Abhishek Joshi <66815782+abhishekjoshi2612@users.noreply.github.com>
2024-07-28 15:29:17 +0530

    Update main.tf

--- Ec2-Instance/terraform variables/main.tf
+++ Ec2-Instance/terraform variables/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+variable "ami_value" { 
+type = string
+ default =  "ami-04a81a99f5ec58529"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+   instance_type = "t3.micro"
+   ami = var.ami_value
+}
