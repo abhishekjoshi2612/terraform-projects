ff36eece02548b7a5ce7d09a823b257b36cfd898
Abhishek Joshi <66815782+abhishekjoshi2612@users.noreply.github.com>
2024-07-29 18:51:05 +0530

    Create variables.tf

---
+++ Ec2-Instance/module_use/create_ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "right_value"{
+    type = string
+/* default =  "right" */
+}
+variable "wrong_ami_value"{
+  type = string
+/* default =  "wrong_value" */
+}
+variable "right_ami_value" { 
+type = string
+ /*default =  "ami-04a81a99f5ec58529"*/
+}
