64eaf09c7f837662e851cce70ccef924cf95f694
Abhishek Joshi <66815782+abhishekjoshi2612@users.noreply.github.com>
2024-08-03 14:31:43 +0530

    added s3 remote backend

--- Ec2-Instance/remote-backend/main.tf
+++ Ec2-Instance/remote-backend/main.tf
@@ -1,1 +1,22 @@
 
+terraform {
+  backend "s3" {
+    bucket = "store-file-transfer"
+    key = "tf.state"
+    encrypt = true
+    region = "us-east-1"
+    dynamodb_table = "terraform-table"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+   instance_type = "t3.micro"
+   ami = "ami-04a81a99f5ec58529"
+      
+}
+
+
